2dd0b0a6a4fea4adef23f83d649f9bac7fa5a590
taraskl <taras.kliuchnyk@gmail.com>
2019-01-31 17:54:58 +0200

    add login, logout, signin forms

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,51 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    user = authenticate(username='john', password='secret')
+    if user is not None:
+        pass
+    else:
+        pass
+
+    context = {
+        "users": User.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+            return render(request, "index.html")
+        else:
+            # Return an 'invalid login' error message.
+            return render(request, "index.html")   
+
+    return render(request, "login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, "index.html")   
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "index.html") 
+    else:        
+        form = UserCreationForm()
+    return render(request, "signup.html", { "form":form }) 
+
