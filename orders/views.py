078e80a2299dd647480c52116c5c9589cc590a34
taraskl <taras.kliuchnyk@gmail.com>
2019-02-05 17:13:30 +0200

    add menu, start virtual cart

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from orders.forms import UserCreationForm
 from django import forms
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
+from orders.forms import UserCreationForm
+from orders.models import Pizza, Subs, Pasta, Salads, Dinner_Platters
 
 # Create your views here.
 def index(request):
@@ -26,7 +27,14 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request, "index.html")
+            context = {
+                "pizza": Pizza.objects.all(),
+                "subs": Subs.objects.all(),
+                "pasta": Pasta.objects.all(),
+                "salads": Salads.objects.all(),
+                "dinner_platters": Dinner_Platters.objects.all(),
+            }
+            return render(request, "menu.html", context)
         else:
             # To do: Return an 'invalid login' error message.
             return render(request, "index.html")   
@@ -47,4 +55,25 @@ def signup_view(request):
         form = UserCreationForm()
 
     return render(request, "signup.html", { "form":form }) 
+
+def menu_view(request):
+    context = {
+        "pizza": Pizza.objects.all(),
+        "subs": Subs.objects.all(),
+        "pasta": Pasta.objects.all(),
+        "salads": Salads.objects.all(),
+        "dinner_platters": Dinner_Platters.objects.all(),
+    }
+    if request.method == "POST":
+        try:
+            regular_pizza_small = request.POST['regular_pizza_small']
+            regular_pizza_large = request.POST['regular_pizza_large']
+        except:
+            pass    
+        topping_1 = request.POST['topping_1']
+        print(regular_pizza_small)
+
+        return render(request, "menu.html", context)
+
+    return render(request, "menu.html", context) 
 
